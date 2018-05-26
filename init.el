736d8442055a0fdf11d67030cdea007f47e2ff33
Joseph Wayne Norton <norton@alum.mit.edu>
2018-05-27 01:03:26 +0900

    Apply fixes for macOS

--- init.el
+++ init.el
@@ -3,6 +3,16 @@
 ;;   - All of the configuration is within `configuration.org`
 ;;; Code:
 
+(require 'gnutls)
+(add-to-list 'gnutls-trustfiles
+             "/usr/local/etc/openssl/cert.pem")
+(add-to-list 'gnutls-trustfiles
+             (expand-file-name
+              "~/.emacs.d/comodo.rsa.ca.intermediate.crt"))
+
+(add-to-list 'load-path
+             "/usr/local/share/emacs/site-lisp/mu/mu4e")
+
 (package-initialize)
 
 ;; This loads the actual configuration in literate org-mode elisp
